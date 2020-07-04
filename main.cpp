549b23846775a93b7314fb0ea80ad1fe48e6861b
Zeyad Khattab <41998602+ZeyadKhattab@users.noreply.github.com>
2020-07-04 22:19:21 +0200

    fixed indentation

--- main.cpp
+++ main.cpp
@@ -8,13 +8,14 @@
 
 #define ABS_THRESH 7.0
 #define REL_THRESH 0.85
+
 std::vector<std::vector<float>> getDisparity(const std::string file_name,bool groundTruth) {
 	std::vector<std::vector<float>> ans;
 	png::image<png::gray_pixel_16> image(file_name);
 	int32_t   width_  = image.get_width();
    	int32_t  height_ = image.get_height();
    	float*  data_   = (float*)malloc(width_*height_*sizeof(float));
-    for (int32_t v=0; v<height_; v++) {
+	for (int32_t v=0; v<height_; v++) {
     	std::vector<float> curr;
       	for (int32_t u=0; u<width_; u++) {
 			uint16_t val = image.get_pixel(u,v);
@@ -23,9 +24,7 @@ std::vector<std::vector<float>> getDisparity(const std::string file_name,bool gr
     	ans.push_back(curr);
     }
     return ans;
-
-
-  }
+}
 void writeDisparityFile (const std::string file_name,bool groundTruth) {
 	std::ofstream myfile;
   	myfile.open ("results/disp.txt");
@@ -43,31 +42,29 @@ void writeDisparityFile (const std::string file_name,bool groundTruth) {
     myfile.close();
 }
 	/* errors, pixels, MSE */
-  std::vector<float> getErrors(std::vector<std::vector<float>>truth, std::vector<std::vector<float>> estimation){
-  	assert(truth.size()==estimation.size());
-  	//assert(truth[0].size()==estimation[0].size()); //applying dopost increases width? 
-  	float pixels=0,errors=0;
-	float minDiff = 1e9;
+std::vector<float> getErrors(std::vector<std::vector<float>>truth, std::vector<std::vector<float>> estimation){
+	assert(truth.size()==estimation.size());
+	//assert(truth[0].size()==estimation[0].size()); //applying dopost increases width? 
+	float pixels=0,errors=0;
 	float SSD=0;
-  	for(int i=0;i<truth.size();i++)
-  		for(int j=0;j<truth[0].size();j++){
-  			if(truth[i][j]==-1) continue;
-  			float truth_value=truth[i][j],estimation_value=estimation[i][j];
-			minDiff=std::min(minDiff,fabs(truth_value-estimation_value));
-  			bool err = fabs(truth_value-estimation_value)>ABS_THRESH && 
-  				fabs(truth_value-estimation_value)/fabs(truth_value)>REL_THRESH;
-  			if(err)
-  				errors++;
-  			pixels++;
+	for(int i=0;i<truth.size();i++)
+		for(int j=0;j<truth[0].size();j++){
+			if(truth[i][j]==-1) continue;
+			float truth_value=truth[i][j],estimation_value=estimation[i][j];
+			bool err = fabs(truth_value-estimation_value)>ABS_THRESH && 
+				fabs(truth_value-estimation_value)/fabs(truth_value)>REL_THRESH;
+			if(err)
+				errors++;
+			pixels++;
 			SSD+=(truth_value-estimation_value)*(truth_value-estimation_value);
-  		}
-	//std::cout<<minDiff<<"\n";
-  	std::vector<float>ans;
-  	ans.push_back(errors);
-  	ans.push_back(pixels);
+		}
+	std::vector<float>ans;
+	ans.push_back(errors);
+	ans.push_back(pixels);
 	ans.push_back(SSD/pixels);
-  	return ans;
-  }
+	return ans;
+}
+  
 std::vector<float> getErrors(const std::string truth_file_name,const std::string estimation_file_name){
 	std::vector<std::vector<float>>truth = getDisparity(truth_file_name,true);
 	std::vector<std::vector<float>> estimation= getDisparity(estimation_file_name,false);
